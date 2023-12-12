8f4010d989f08f1bb5eb5f7ee3547eb24fc2c898
FSE_VR23 <FSE_VR23@LAPTOP-OE4HKTPU>
2023-12-12 15:05:50 +0000

    Implemented random navigation for gorilla navmesh agent.

--- Assets/Scripts/Gorilla.cs
+++ Assets/Scripts/Gorilla.cs
@@ -6,13 +6,17 @@ using UnityEngine.AI;
 
 public class Gorilla : MonoBehaviour
 {
+    public NavMeshAgent agent;
     public float wanderDistance;
+    private Vector3 targetPosition;
 
+    private float _waitTime;
+    private float _waitTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _waitTime = 5f;
     }
 
     // Update is called once per frame
@@ -20,13 +24,20 @@ public class Gorilla : MonoBehaviour
     {
 
         // Random NavMesh position
-
-        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * wanderDistance;
-        randomDirection += transform.position;
-        NavMeshHit navHit;
-        NavMesh.SamplePosition(randomDirection, out navHit, wanderDistance, NavMesh.AllAreas);
-        // targetPosition = navHit.position;
-        // agent.SetDestination(targetPosition);
-        // _waitTimer = 0f;
+        if (_waitTimer < _waitTime)
+        {
+            _waitTimer += Time.deltaTime;
+        }
+        else
+        {
+            Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * wanderDistance;
+            randomDirection += transform.position;
+            NavMeshHit navHit;
+            NavMesh.SamplePosition(randomDirection, out navHit, wanderDistance, NavMesh.AllAreas);
+            targetPosition = navHit.position;
+            agent.SetDestination(targetPosition);
+            _waitTimer = 0f;
+        }
+        
     }
 }
