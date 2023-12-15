e96dc2337daacb19e31e4ea4404fa09202815fe1
FSE_VR23 <FSE_VR23@LAPTOP-OE4HKTPU>
2023-12-15 15:37:17 +0000

    Added some gorilla-banana interaction

---
+++ Assets/Scripts/GorillaSceneManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GorillaSceneManager : MonoBehaviour
+{
+    public List<Gorilla> gorillas = new List<Gorilla>();
+    public Human human;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if all gorillas have bananas
+        foreach (Gorilla g in gorillas) if(!g.HasBanana()){
+            return;
+        }
+
+        // this code won't run untill all gorillas have bananas
+        human.gameObject.SetActive(true);
+        foreach (Gorilla g in gorillas) { 
+            g.AttackHuman();
+        }
+    }
+}
