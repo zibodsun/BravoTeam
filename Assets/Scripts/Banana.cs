d637621b6b4da31faf3acf29c450374a11acf730
sundzibo <d.z.sun@lancaster.ac.uk>
2024-01-20 13:06:50 +0000

    Made bananas reset when they fall out of bounds of the floor, and added text UI with instructions for the user.

---
+++ Assets/Scripts/Banana.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banana : MonoBehaviour
+{
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -10) {
+            transform.position = startPosition;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
